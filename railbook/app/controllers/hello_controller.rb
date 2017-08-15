61e0332067936200c6f3ab4983350709f6360e4c
marshmallow <marshmallow0319@gmail.com>
2017-08-15 20:17:18 +0900

    view への変数の渡し方復習

--- railbook/app/controllers/hello_controller.rb
+++ railbook/app/controllers/hello_controller.rb
@@ -1,10 +1,14 @@
 class HelloController < ApplicationController
- def index
-  render plain: 'こんにちは、世界！'
- end
+  def index
+    render plain: 'こんにちは、世界！'
+  end
 
- def view
-  @msg = 'こんにちは、世界！'
-  render 'hello/view'
- end
+  def view
+    @msg = 'こんにちは、世界！'
+    render 'hello/view'
+  end
+
+  def list
+    @books = Book.all
+  end
 end
