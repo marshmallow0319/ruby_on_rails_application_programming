910a36cc29f94817f844c1b92179dcc2e71abc66
marshmallow <marshmallow0319@gmail.com>
2017-08-15 20:28:59 +0900

    rails db:migrate

--- railbook/db/schema.rb
+++ railbook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815093637) do
+ActiveRecord::Schema.define(version: 20170815112435) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "isbn"
